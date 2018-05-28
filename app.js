fc6490994bae41833b9983b1971d38dccf4c7594
Florent Plomb <florent.plomb@novaccess.ch>
2018-05-28 13:12:01 +0200

    update for openschift

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
 'use strict';
 
 var express = require('express');
 var app = express()
-var port = process.env.PORT || 3000;
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/public');
